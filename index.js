f7f97e7aa63b137504d6fcf675aaa9d8a51489b2
Monnuery Júnior <monnuerypachecodossantos@gmail.com>
2023-09-20 12:35:32 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const server = express();
+
+server.use(express.json());
+
+//Query Params = ?nome=NodeJS
+
+//Route Params = /cursos/2
+
+//Request Body = {nome: 'lala'}
+
+//CRUD > Create, Read, Update, Delete
+
+const cursos = ["Node JS", "JavaScript", "React Native"];
+
+//Middleware Global
+server.use((req, res, next) => {
+  console.log(`Requisição: ${req.url}`);
+  return next();
+});
+
+function checkCurso(req, res, next) {
+  if (!req.body.name) {
+    return res.status(400).json({ error: "Nome do curso é obrigatório" });
+  }
+  return next();
+}
+
+function checkIndex(req, res, next) {
+  const curso = cursos[req.params.index];
+  if (!curso) {
+    return res.status(400).json({ error: "o curso não existe" });
+  }
+  return next();
+}
+
+server.get("/cursos", (req, res) => {
+  return res.json(cursos);
+});
+
+server.get("/cursos/:index", checkIndex, (req, res) => {
+  const { index } = req.params;
+  return res.json(cursos[index]);
+});
+
+server.post("/cursos", checkCurso, (req, res) => {
+  const { name } = req.body;
+  cursos.push(name);
+  return res.json(cursos);
+});
+
+server.put("/cursos/:index", checkCurso, checkIndex, (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+  cursos[index] = name;
+  return res.json(cursos);
+});
+server.listen(3000);
+
+server.delete("/cursos/:index", checkIndex, (req, res) => {
+  const { index } = req.params;
+  cursos.splice(index, 1);
+  return res.json(cursos);
+});
